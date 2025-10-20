749d81038a955a4e5ff0e0ccf8df5beac027db4d
eloimaduell <eloi@playmodes.com>
2025-10-21 01:49:05 +0200

    [textureRecorder] : adapting textureRecorder to Alpha . Added a parameter to record in Alpha mode. [TO DO : Save video script debug and add alpha capabilties]

--- src/textureRecorder.cpp
+++ src/textureRecorder.cpp
@@ -15,6 +15,8 @@ textureRecorder::textureRecorder() : ofxOceanodeNodeModel("Texture Recorder"){
     addParameter(filename.set("File", "recTest"));
     addParameter(input.set("Input", nullptr));
     
+    addParameter(recordAlpha.set("Alpha?", false));
+    
     addInspectorParameter(createVideo.set("Create Video", false));
 
     listeners.push(phasorIn.newListener(this, &textureRecorder::phasorInListener));
@@ -45,9 +47,13 @@ void textureRecorder::inputListener(ofTexture* &texture){
         if(!recorderIsSetup || input.get()->getWidth() != width || input.get()->getHeight() != height){
             width = input.get()->getWidth();
             height = input.get()->getHeight();
-            fbo.allocate(width, height, GL_RGB);
+            fbo.allocate(width, height, recordAlpha ? GL_RGBA : GL_RGB);
             fbo.begin();
-            ofClear(0);
+            if(recordAlpha){
+                ofClear(0, 0, 0, 0);  // Clear with full transparency for alpha
+            }else{
+                ofClear(0, 0, 0, 255);  // Clear with opaque black for RGB
+            }
             input.get()->draw(0,0);
             fbo.end();
             recorderIsSetup = true;
@@ -55,10 +61,29 @@ void textureRecorder::inputListener(ofTexture* &texture){
         if(record){
             if(input != nullptr){
                 fbo.begin();
+                if(recordAlpha){
+                    ofClear(0, 0, 0, 0);  // Clear with full transparency before each frame
+                }else{
+                    ofClear(0, 0, 0, 255);  // Clear with opaque black before each frame
+                }
                 input.get()->draw(0,0);
                 fbo.end();
                 ofPixels pixels;
                 fbo.getTexture().readToPixels(pixels);
+                
+                // Convert from premultiplied to straight alpha if recording with alpha
+                if(recordAlpha && pixels.getNumChannels() == 4){
+                    for(int i = 0; i < pixels.size(); i += 4){
+                        float alpha = pixels[i + 3] / 255.0f;
+                        if(alpha > 0.0f){
+                            // Unpremultiply: divide RGB by alpha
+                            pixels[i] = ofClamp(pixels[i] / alpha, 0, 255);
+                            pixels[i + 1] = ofClamp(pixels[i + 1] / alpha, 0, 255);
+                            pixels[i + 2] = ofClamp(pixels[i + 2] / alpha, 0, 255);
+                        }
+                    }
+                }
+                
                 ofSaveImage(pixels, "recordings/" + filename.get() +  "_" + initRecordingTimestamp + "/" + filename.get() + "_" + ofToString(frameCounter, 9, '0') + ".png");
 //                image.clear();
             }
@@ -82,7 +107,11 @@ void textureRecorder::recordListener(bool &b){
         setFlags(ofxOceanodeNodeModelFlags_None);
         if(createVideo){
             string command = "cd " + ofToDataPath("recordings/" + filename.get() +  "_" + initRecordingTimestamp, true);
-            command += " && /opt/homebrew/bin/ffmpeg -f image2 -framerate " + ofToString(ofGetTargetFrameRate()) + " -pattern_type glob -i '*.png' -c:v prores_ks -profile:v 4 " + filename.get() + ".mov";
+            if(recordAlpha){
+                command += " && /opt/homebrew/bin/ffmpeg -f image2 -framerate " + ofToString(ofGetTargetFrameRate()) + " -pattern_type glob -i '*.png' -c:v prores_ks -profile:v 4444 -pix_fmt yuva444p10le " + filename.get() + ".mov";
+            }else{
+                command += " && /opt/homebrew/bin/ffmpeg -f image2 -framerate " + ofToString(ofGetTargetFrameRate()) + " -pattern_type glob -i '*.png' -c:v prores_ks -profile:v 4 " + filename.get() + ".mov";
+            }
             system(command.c_str());
         }
     }
